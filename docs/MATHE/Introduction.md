35200e63fe5faeef8f037cd500da2fc083a0c176
Shi Jingyuan <129636262+Eason193-sketch@users.noreply.github.com>
2024-05-18 15:40:19 +0800

    no

--- docs/MATHE/Introduction.md
+++
@@ -1,7 +0,0 @@
-# 一些推荐的教程
-
-## 有限元法
-
-* Brenner&Scott : The mathematical theory of finite element methods. Braess: Finite elements
-* Stroustrup: The C++ programming language
-
--- site/assets/javascripts/titlechange.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('visibilitychange', function () {
-    if (document.visibilityState == 'hidden') {
-        normal_title = "(≧∀≦)♪你回来啦！";
-        document.title = '!!!∑(ﾟДﾟノ)ノ网页未响应...';
-    } else document.title = normal_title;
-});
